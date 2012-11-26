c0bdda50f652c5e9403bb3c7d62f4bcb6460fb42
Paul Welter <pwelter@loresoft.com>
2012-11-26 12:58:56 -0600

    - strong name Assembly

--- Source/EntityFramework.Extended/Properties/AssemblyInfo.cs
+++ Source/EntityFramework.Extended/Properties/AssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("642461ab-b078-4b69-a0ca-4d34bbbc6113")]
-[assembly: InternalsVisibleTo("EntityFramework.Extended.Test")]
+[assembly: InternalsVisibleTo("EntityFramework.Extended.Test, PublicKey=00240000048000009400000006020000002400005253413100040000010001003d79b4c6a55efcd967124f1689973c706f25ef8916bcc19525cc6d13625f009801888a5e94ea78364537a973e525c0413e0a2ecb415d690a21f4c04e56a1e658d744862957144637cf642f89caf97185bb4c5b46051de4cb315ace156548c7a2f0e6234005a8cbb76b7532677b2a4fb5cbda1debcd30c916e87e38deeff918c7")]
