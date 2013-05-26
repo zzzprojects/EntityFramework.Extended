3be94e79c8dbddd5fd9ef53e312366371321b79e
Stephen Reindl <stephen@think2infinity.com>
2013-05-26 23:00:59 +0200

    #24: Allow ComplexType properties in batch update

--- Source/EntityFramework.Extended/Batch/SqlServerBatchRunner.cs
+++ Source/EntityFramework.Extended/Batch/SqlServerBatchRunner.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.EntityClient;
 using System.Data.Objects;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Dynamic;
 using System.Linq.Expressions;
@@ -170,112 +172,28 @@ namespace EntityFramework.Batch
                 bool wroteSet = false;
                 foreach (MemberBinding binding in memberInitExpression.Bindings)
                 {
-                    if (wroteSet)
-                        sqlBuilder.AppendLine(", ");
-
-                    string propertyName = binding.Member.Name;
-                    string columnName = entityMap.PropertyMaps
-                        .Where(p => p.PropertyName == propertyName)
-                        .Select(p => p.ColumnName)
-                        .FirstOrDefault();
-
-
-                    var memberAssignment = binding as MemberAssignment;
-                    if (memberAssignment == null)
-                        throw new ArgumentException("The update expression MemberBinding must only by type MemberAssignment.", "updateExpression");
-
-                    Expression memberExpression = memberAssignment.Expression;
-
-                    ParameterExpression parameterExpression = null;
-                    memberExpression.Visit((ParameterExpression p) =>
+                    IPropertyMapElement propertyMap =
+                        entityMap.PropertyMaps.SingleOrDefault(p => p.PropertyName == binding.Member.Name);
+                    if (propertyMap is ComplexPropertyMap)
                     {
-                        if (p.Type == entityMap.EntityType)
-                            parameterExpression = p;
-
-                        return p;
-                    });
-
-
-                    if (parameterExpression == null)
-                    {
-                        object value;
-
-                        if (memberExpression.NodeType == ExpressionType.Constant)
+                        ComplexPropertyMap cpm = propertyMap as ComplexPropertyMap;
+                        var memberAssignment = binding as MemberAssignment;
+                        if (memberAssignment == null)
+                            throw new ArgumentException("The update expression MemberBinding must only by type MemberAssignment.", "updateExpression");
+                        var expr = memberAssignment.Expression as MemberInitExpression;
+                        if (expr == null)
+                            throw new ArgumentException("The update expression MemberBinding must only by type MemberAssignment.", "updateExpression");
+                        foreach (var subBinding in expr.Bindings)
                         {
-                            var constantExpression = memberExpression as ConstantExpression;
-                            if (constantExpression == null)
-                                throw new ArgumentException(
-                                    "The MemberAssignment expression is not a ConstantExpression.", "updateExpression");
-
-                            value = constantExpression.Value;
-                        }
-                        else
-                        {
-                            LambdaExpression lambda = Expression.Lambda(memberExpression, null);
-                            value = lambda.Compile().DynamicInvoke();
-                        }
-
-                        if (value != null)
-                        {
-                            string parameterName = "p__update__" + nameCount++;
-                            var parameter = updateCommand.CreateParameter();
-                            parameter.ParameterName = parameterName;
-                            parameter.Value = value;
-                            updateCommand.Parameters.Add(parameter);
-
-                            sqlBuilder.AppendFormat("[{0}] = @{1}", columnName, parameterName);
-                        }
-                        else
-                        {
-                            sqlBuilder.AppendFormat("[{0}] = NULL", columnName);
+                            AddUpdateRow<TEntity>(objectContext, entityMap, subBinding, sqlBuilder, updateCommand,
+                                                  cpm.TypeElements, ref nameCount, ref wroteSet);
                         }
                     }
                     else
                     {
-                        // create clean objectset to build query from
-                        var objectSet = objectContext.CreateObjectSet<TEntity>();
-
-                        Type[] typeArguments = new[] { entityMap.EntityType, memberExpression.Type };
-
-                        ConstantExpression constantExpression = Expression.Constant(objectSet);
-                        LambdaExpression lambdaExpression = Expression.Lambda(memberExpression, parameterExpression);
-
-                        MethodCallExpression selectExpression = Expression.Call(
-                            typeof(Queryable),
-                            "Select",
-                            typeArguments,
-                            constantExpression,
-                            lambdaExpression);
-
-                        // create query from expression
-                        var selectQuery = objectSet.CreateQuery(selectExpression, entityMap.EntityType);
-                        string sql = selectQuery.ToTraceString();
-
-                        // parse select part of sql to use as update
-                        string regex = @"SELECT\s*\r\n(?<ColumnValue>.+)?\s*AS\s*(?<ColumnAlias>\[\w+\])\r\nFROM\s*(?<TableName>\[\w+\]\.\[\w+\]|\[\w+\])\s*AS\s*(?<TableAlias>\[\w+\])";
-                        Match match = Regex.Match(sql, regex);
-                        if (!match.Success)
-                            throw new ArgumentException("The MemberAssignment expression could not be processed.", "updateExpression");
-
-                        string value = match.Groups["ColumnValue"].Value;
-                        string alias = match.Groups["TableAlias"].Value;
-
-                        value = value.Replace(alias + ".", "");
-
-                        foreach (ObjectParameter objectParameter in selectQuery.Parameters)
-                        {
-                            string parameterName = "p__update__" + nameCount++;
-
-                            var parameter = updateCommand.CreateParameter();
-                            parameter.ParameterName = parameterName;
-                            parameter.Value = objectParameter.Value;
-                            updateCommand.Parameters.Add(parameter);
-
-                            value = value.Replace(objectParameter.Name, parameterName);
-                        }
-                        sqlBuilder.AppendFormat("[{0}] = {1}", columnName, value);
+                        AddUpdateRow<TEntity>(objectContext, entityMap, binding, sqlBuilder, updateCommand,
+                                              entityMap.PropertyMaps, ref nameCount, ref wroteSet);
                     }
-                    wroteSet = true;
                 }
 
                 sqlBuilder.AppendLine(" ");
@@ -314,6 +232,116 @@ namespace EntityFramework.Batch
                 if (updateConnection != null && ownConnection)
                     updateConnection.Close();
             }
+        }
+
+
+        private static void AddUpdateRow<TEntity>(ObjectContext objectContext, EntityMap entityMap, MemberBinding binding, StringBuilder sqlBuilder, DbCommand updateCommand, IEnumerable<IPropertyMapElement> propertyMap, ref int nameCount, ref bool wroteSet)
+            where TEntity : class
+        {
+            if (wroteSet)
+                sqlBuilder.AppendLine(", ");
+
+            string propertyName = binding.Member.Name;
+            PropertyMap property =
+                propertyMap.SingleOrDefault(p => p.PropertyName == propertyName) as PropertyMap;
+            Debug.Assert(property != null, "property != null");
+            string columnName = property.ColumnName;
+            var memberAssignment = binding as MemberAssignment;
+            if (memberAssignment == null)
+                throw new ArgumentException("The update expression MemberBinding must only by type MemberAssignment.", "binding");
+
+            Expression memberExpression = memberAssignment.Expression;
+
+            ParameterExpression parameterExpression = null;
+            memberExpression.Visit((ParameterExpression p) =>
+            {
+                if (p.Type == entityMap.EntityType)
+                    parameterExpression = p;
+
+                return p;
+            });
+
+
+            if (parameterExpression == null)
+            {
+                object value;
+
+                if (memberExpression.NodeType == ExpressionType.Constant)
+                {
+                    var constantExpression = memberExpression as ConstantExpression;
+                    if (constantExpression == null)
+                        throw new ArgumentException(
+                            "The MemberAssignment expression is not a ConstantExpression.", "binding");
+
+                    value = constantExpression.Value;
+                }
+                else
+                {
+                    LambdaExpression lambda = Expression.Lambda(memberExpression, null);
+                    value = lambda.Compile().DynamicInvoke();
+                }
+
+                if (value != null)
+                {
+                    string parameterName = "p__update__" + nameCount++;
+                    var parameter = updateCommand.CreateParameter();
+                    parameter.ParameterName = parameterName;
+                    parameter.Value = value;
+                    updateCommand.Parameters.Add(parameter);
+
+                    sqlBuilder.AppendFormat("[{0}] = @{1}", columnName, parameterName);
+                }
+                else
+                {
+                    sqlBuilder.AppendFormat("[{0}] = NULL", columnName);
+                }
+            }
+            else
+            {
+                // create clean objectset to build query from
+                var objectSet = objectContext.CreateObjectSet<TEntity>();
+
+                Type[] typeArguments = new[] { entityMap.EntityType, memberExpression.Type };
+
+                ConstantExpression constantExpression = Expression.Constant(objectSet);
+                LambdaExpression lambdaExpression = Expression.Lambda(memberExpression, parameterExpression);
+
+                MethodCallExpression selectExpression = Expression.Call(
+                    typeof(Queryable),
+                    "Select",
+                    typeArguments,
+                    constantExpression,
+                    lambdaExpression);
+
+                // create query from expression
+                var selectQuery = objectSet.CreateQuery(selectExpression, entityMap.EntityType);
+                string sql = selectQuery.ToTraceString();
+
+                // parse select part of sql to use as update
+                const string regex = @"SELECT\s*\r\n(?<ColumnValue>.+)?\s*AS\s*(?<ColumnAlias>\[\w+\])\r\nFROM\s*(?<TableName>\[\w+\]\.\[\w+\]|\[\w+\])\s*AS\s*(?<TableAlias>\[\w+\])";
+                Match match = Regex.Match(sql, regex);
+                if (!match.Success)
+                    throw new ArgumentException("The MemberAssignment expression could not be processed.", "binding");
+
+                string value = match.Groups["ColumnValue"].Value;
+                string alias = match.Groups["TableAlias"].Value;
+
+                value = value.Replace(alias + ".", "");
+
+                foreach (ObjectParameter objectParameter in selectQuery.Parameters)
+                {
+                    string parameterName = "p__update__" + nameCount++;
+
+                    var parameter = updateCommand.CreateParameter();
+                    parameter.ParameterName = parameterName;
+                    parameter.Value = objectParameter.Value;
+                    updateCommand.Parameters.Add(parameter);
+
+                    value = value.Replace(objectParameter.Name, parameterName);
+                }
+                sqlBuilder.AppendFormat("[{0}] = {1}", columnName, value);
+            }
+            wroteSet = true;
         }
 
         private static Tuple<DbConnection, DbTransaction> GetStore(ObjectContext objectContext)
---
+++ Source/EntityFramework.Extended/Mapping/IPropertyMapElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Mapping
+{
+    /// <summary>
+    /// Interface representing a property map element which can be single or complex
+    /// </summary>
+    public interface IPropertyMapElement
+    {
+        /// <summary>
+        /// Gets or sets the name of the property.
+        /// </summary>
+        string PropertyName { get; set; }
+    }
+}
