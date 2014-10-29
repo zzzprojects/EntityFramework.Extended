d5aa5039e81d9bbaeaf1ecbaab1e41d01277cd00
cocowalla <colin.anderson333@gmail.com>
2014-10-29 13:16:08 +0000

    Fix for #14. Use hash of SQL as cache key
    Fix for #14. Build cache key from a hash of the SQL that will be
    executed, along with any parameters and their values.
    
    With the previous expression-based method, parameters and their values
    were *not* being included in the cache key when `Include()` was used.

--- Source/EntityFramework.Extended/Extensions/ObjectQueryExtensions.cs
+++ Source/EntityFramework.Extended/Extensions/ObjectQueryExtensions.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Entity.Core.Objects;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
 using EntityFramework.Reflection;
 
 namespace EntityFramework.Extensions
@@ -11,126 +11,156 @@ namespace EntityFramework.Extensions
     /// <summary>
     /// Extension methods for for ObjectQuery.
     /// </summary>
-  public static class ObjectQueryExtensions
-  {
-      /// <summary>
-      /// Convert the query into an ObjectQuery.
-      /// </summary>
-      /// <typeparam name="TEntity">The type of the entity.</typeparam>
-      /// <param name="query">The query to convert.</param>
-      /// <returns>The converted ObjectQuery; otherwise null if it can't be converted.</returns>
-      public static ObjectQuery<TEntity> ToObjectQuery<TEntity>(this IQueryable<TEntity> query) where TEntity : class
-      {
-          // first try direct cast
-          var objectQuery = query as ObjectQuery<TEntity>;
-          if (objectQuery != null)
-              return objectQuery;
+    public static class ObjectQueryExtensions
+    {
+        /// <summary>
+        /// Get the SQL that the query will run, along with the parameter values
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="query">The query to get the SQL for.</param>
+        /// <returns>The SQL that the query will run, along with the parameter values</returns>
+        public static string ToTraceString<TEntity>(this IQueryable<TEntity> query) where TEntity : class
+        {
+            var sb = new StringBuilder();
+            var objectQuery = query.ToObjectQuery();
 
-          // next try case to DbQuery
-          var dbQuery = query as DbQuery<TEntity>;
+            sb.AppendLine(query.ToString());
 
-          // next look for an ObjectQuery property
-          if (dbQuery == null)
-          {
-              var queryType = query.GetType();
-              var prop = queryType.GetProperties().FirstOrDefault(p => p.PropertyType == typeof(ObjectQuery));
+            foreach (var parm in objectQuery.Parameters)
+            {
+                sb.AppendFormat("{0}: {1}", parm.Name, parm.Value).AppendLine();
+            }
 
-              if (prop == null)
-                  return null;
+            return sb.ToString();
+        }
 
-              return (ObjectQuery<TEntity>)prop.GetValue(query, null);
-          }
+        /// <summary>
+        /// Convert the query into an ObjectQuery.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="query">The query to convert.</param>
+        /// <returns>The converted ObjectQuery; otherwise null if it can't be converted.</returns>
+        public static ObjectQuery<TEntity> ToObjectQuery<TEntity>(this IQueryable<TEntity> query) where TEntity : class
+        {
+            // first try direct cast
+            var objectQuery = query as ObjectQuery<TEntity>;
+            if (objectQuery != null)
+                return objectQuery;
 
-          // access internal property InternalQuery
-          dynamic dbQueryProxy = new DynamicProxy(dbQuery);
-          dynamic internalQuery = dbQueryProxy.InternalQuery;
-          if (internalQuery == null)
-              return null;
+            // next try case to DbQuery
+            var dbQuery = query as DbQuery<TEntity>;
 
-          // access internal property ObjectQuery
-          dynamic objectQueryProxy = internalQuery.ObjectQuery;
-          if (objectQueryProxy == null)
-              return null;
+            // next look for an ObjectQuery property
+            if (dbQuery == null)
+            {
+                var queryType = query.GetType();
+                var prop = queryType.GetProperties().FirstOrDefault(p => p.PropertyType == typeof(ObjectQuery));
 
-          // convert dynamic to static type
-          objectQuery = objectQueryProxy;
-          return objectQuery;
-      }
+                if (prop == null)
+                    return null;
 
-      /// <summary>
-      /// Convert the query into an ObjectQuery.
-      /// </summary>
-      /// <param name="query">The query to convert.</param>
-      /// <returns>The converted ObjectQuery; otherwise null if it can't be converted.</returns>
-      public static ObjectQuery ToObjectQuery(this IQueryable query)
-      {
-          // first try direct cast
-          var objectQuery = query as ObjectQuery;
-          if (objectQuery != null)
-              return objectQuery;
+                return (ObjectQuery<TEntity>)prop.GetValue(query, null);
+            }
 
-          // next try case to DbQuery
-          var dbQuery = query as DbQuery;
-          if (dbQuery == null)
-              return null;
+            // access internal property InternalQuery
+            dynamic dbQueryProxy = new DynamicProxy(dbQuery);
+            dynamic internalQuery = dbQueryProxy.InternalQuery;
+            if (internalQuery == null)
+                return null;
 
-          // access internal property InternalQuery
-          dynamic dbQueryProxy = new DynamicProxy(dbQuery);
-          dynamic internalQuery = dbQueryProxy.InternalQuery;
-          if (internalQuery == null)
-              return null;
+            // access internal property ObjectQuery
+            dynamic objectQueryProxy = internalQuery.ObjectQuery;
+            if (objectQueryProxy == null)
+                return null;
 
-          // access internal property ObjectQuery
-          dynamic objectQueryProxy = internalQuery.ObjectQuery;
-          if (objectQueryProxy == null)
-              return null;
+            // convert dynamic to static type
+            objectQuery = objectQueryProxy;
+            return objectQuery;
+        }
 
-          // convert dynamic to static type
-          objectQuery = objectQueryProxy;
-          return objectQuery;
-      }
+        /// <summary>
+        /// Convert the query into an ObjectQuery.
+        /// </summary>
+        /// <param name="query">The query to convert.</param>
+        /// <returns>The converted ObjectQuery; otherwise null if it can't be converted.</returns>
+        public static ObjectQuery ToObjectQuery(this IQueryable query)
+        {
+            // first try direct cast
+            var objectQuery = query as ObjectQuery;
+            if (objectQuery != null)
+                return objectQuery;
 
-      /// <summary>
-      /// Creates an ObjectQuery from an expression.
-      /// </summary>
-      /// <param name="source">The source.</param>
-      /// <param name="expression">The expression.</param>
-      /// <param name="type">The type.</param>
-      /// <returns>An ObjectQuery created from the expression.</returns>
-      public static ObjectQuery CreateQuery(this IQueryable source, Expression expression, Type type)
-      {
-          // first convert to object query to get the correct provider
-          ObjectQuery sourceQuery = source.ToObjectQuery();
-          if (sourceQuery == null)
-              return null;
+            // next try case to DbQuery
+            var dbQuery = query as DbQuery;
 
-          IQueryProvider provider = ((IQueryable)sourceQuery).Provider;
+            // next look for an ObjectQuery property
+            if (dbQuery == null)
+            {
+                var queryType = query.GetType();
+                var prop = queryType.GetProperties().FirstOrDefault(p => p.PropertyType == typeof(ObjectQuery));
 
-          // create query from expression using internal ObjectQueryProvider
-          dynamic providerProxy = new DynamicProxy(provider);
-          IQueryable expressionQuery = providerProxy.CreateQuery(expression, type);
-          if (expressionQuery == null)
-              return null;
+                if (prop == null)
+                    return null;
 
-          // convert back to ObjectQuery
-          ObjectQuery resultQuery = expressionQuery.ToObjectQuery();
-          return resultQuery;
-      }
+                return (ObjectQuery)prop.GetValue(query, null);
+            }
 
-      /// <summary>
-      /// Gets the ObjectContext for the specifed query.
-      /// </summary>
-      /// <typeparam name="TEntity">The type of the entity.</typeparam>
-      /// <param name="query">The query.</param>
-      /// <returns>The ObjectContext for the query.</returns>
-      public static ObjectContext GetContext<TEntity>(this IQueryable<TEntity> query) where TEntity : class
-      {
-          var objectQuery = query.ToObjectQuery();
-          if (objectQuery != null)
-              return objectQuery.Context;
+            // access internal property InternalQuery
+            dynamic dbQueryProxy = new DynamicProxy(dbQuery);
+            dynamic internalQuery = dbQueryProxy.InternalQuery;
+            if (internalQuery == null)
+                return null;
 
-          return null;
-      }
+            // access internal property ObjectQuery
+            dynamic objectQueryProxy = internalQuery.ObjectQuery;
+            if (objectQueryProxy == null)
+                return null;
 
-  }
+            // convert dynamic to static type
+            objectQuery = objectQueryProxy;
+            return objectQuery;
+        }
+
+        /// <summary>
+        /// Creates an ObjectQuery from an expression.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="expression">The expression.</param>
+        /// <param name="type">The type.</param>
+        /// <returns>An ObjectQuery created from the expression.</returns>
+        public static ObjectQuery CreateQuery(this IQueryable source, Expression expression, Type type)
+        {
+            // first convert to object query to get the correct provider
+            ObjectQuery sourceQuery = source.ToObjectQuery();
+            if (sourceQuery == null)
+                return null;
+
+            IQueryProvider provider = ((IQueryable)sourceQuery).Provider;
+
+            // create query from expression using internal ObjectQueryProvider
+            dynamic providerProxy = new DynamicProxy(provider);
+            IQueryable expressionQuery = providerProxy.CreateQuery(expression, type);
+            if (expressionQuery == null)
+                return null;
+
+            // convert back to ObjectQuery
+            ObjectQuery resultQuery = expressionQuery.ToObjectQuery();
+            return resultQuery;
+        }
+
+        /// <summary>
+        /// Gets the ObjectContext for the specifed query.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="query">The query.</param>
+        /// <returns>The ObjectContext for the query.</returns>
+        public static ObjectContext GetContext<TEntity>(this IQueryable<TEntity> query) where TEntity : class
+        {
+            var objectQuery = query.ToObjectQuery();
+            if (objectQuery != null)
+                return objectQuery.Context;
+
+            return null;
+        }
+    }
 }
