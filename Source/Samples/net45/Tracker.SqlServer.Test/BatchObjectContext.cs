7ee20e11c41a3696b5b4acbff8e836cbe807c10a
Paul Welter <pwelter@loresoft.com>
2015-07-09 11:03:11 -0500

    add null check tests

--- Source/Samples/net45/Tracker.SqlServer.Test/BatchObjectContext.cs
+++ Source/Samples/net45/Tracker.SqlServer.Test/BatchObjectContext.cs
@@ -20,12 +20,38 @@ namespace Tracker.SqlServer.Test
         }
 
         [Fact]
+        public void DeleteWhereWithExpressionContainingNullParameter()
+        {
+            // This test verifies that the delete is processed correctly when the where expression uses a parameter with a null parameter
+            var db = new TrackerEntities();
+            string emailDomain = "@test.com";
+            string optionalComparisonString = null;
+
+            int count = db.Users
+                .Where(u => (string.IsNullOrEmpty(optionalComparisonString) || u.AvatarType == optionalComparisonString) && u.EmailAddress.EndsWith(emailDomain))
+                .Delete();
+        }
+
+        [Fact]
         public void Update()
         {
             var db = new TrackerEntities();
             string emailDomain = "@test.com";
             int count = db.Users
                 .Where(u => u.EmailAddress.EndsWith(emailDomain))
+                .Update(u => new User { IsApproved = false, LastActivityDate = DateTime.Now });
+        }
+
+        [Fact]
+        public void UpdateWithExpressionContainingNullParameter()
+        {
+            // This test verifies that the update is interpreted correctly when the where expression uses a parameter with a null parameter
+            var db = new TrackerEntities();
+            string emailDomain = "@test.com";
+            string optionalComparisonString = null;
+
+            int count = db.Users
+                .Where(u => u.EmailAddress.EndsWith(emailDomain) && (string.IsNullOrEmpty(optionalComparisonString) || u.AvatarType == optionalComparisonString))
                 .Update(u => new User { IsApproved = false, LastActivityDate = DateTime.Now });
         }
     }
